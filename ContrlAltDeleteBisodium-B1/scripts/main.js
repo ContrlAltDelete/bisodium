02394b40cd731b2479167608546953c9ebc3fc9d
ContrlAltDelete <druzavalli@gmail.com>
2025-05-20 12:41:39 +0300

    Add files via upload

---
+++ ContrlAltDeleteBisodium-B1/scripts/main.js
@@ -0,0 +1,5 @@
+const landpoint.mine = "mine [stat]sodium [white]for build majority of the buildings"
+const landpoint.drillinfo = "[stat]sodium drills [white]can mine []sodium []research them in the research tree"
+const landpoint.drillplace = "place [stat]sodium drills [white]to mine []sodium"
+const landpoint.conveyorplin = "research [stat] sodium conveyor and use them to guide the way from [stat]sodium drills [white]to []core Unity [white]for transport [stat]sodium"
+const landpoint.attack = "attack the [red]enemy core [white] with your [stat]core unit []* In B3, you will not be able to attack the enemy's core with a core unit. *"
